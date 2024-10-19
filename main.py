3f5f37bc723567e1cdefbd3288abb6b82c3d222d
graminio@outlook.com <graminio@outlook.com>
2024-10-19 14:01:37 +0530

    New API Project

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, redirect, render_template
+import json
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('home/index.html')
+
+
+if __name__ == '__main__':
+    app.run()
+
